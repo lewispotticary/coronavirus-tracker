4913ad7616fe1bf431a344a4f63a585bdd4b59f3
Lewis Potticary <lewispotticary@gmail.com>
2021-09-28 23:28:56 +0100

    Fetch countries from API and store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
+
 import './App.css';
 
+//Material UI 
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+
 function App() {
+
+  //useState
+
+  const [countries, setCountries] = useState([]);
+
+  //useEffect
+
+  useEffect(() => {
+    fetch("https://api.covid19api.com/countries")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const Country = data.map(item => item.Slug);
+        setCountries(Country);    
+      })
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Typography variant="h4" m={2}>Covid-19 Statistics By Country</Typography>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Country</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          label="Country"
+          autoWidth
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      
     </div>
   );
 }
