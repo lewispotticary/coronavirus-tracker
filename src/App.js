48641538d258944f252985bc7be62fe38695515d
Lewis Potticary <lewispotticary@gmail.com>
2021-09-28 23:31:33 +0100

    Sorted countries by alphabetical order

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       })
       .then(data => {
         const Country = data.map(item => item.Slug);
+        Country.sort();
         setCountries(Country);    
       })
   },[]);
