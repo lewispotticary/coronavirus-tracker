e1b64895fb20b41c574535afce587ef743aeb3aa
Lewis Potticary <lewispotticary@gmail.com>
2021-10-14 21:40:03 +0100

    Formatted code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,41 +7,26 @@ import styles from './Cards.module.css'
 function Cards({countryInfo}) {
     return (
         <div className={styles.cardContainer}>
-            <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3}>
+            <Grid container justify="center">
+                <Grid item component={Card} xs={12} md={3.15} className={styles.card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">
-                            <CountUp 
-                                start={0}
-                                end={countryInfo.cases}
-                                duration={2.5}
-                                separator=","
-                            />
+                        <Typography variant="h5"><CountUp start={0} end={countryInfo.cases} duration={2.5} separator=","/>
                         </Typography>
-                        <Typography color="textSecondary" gutterBottom>Today Infected:
-                            <CountUp 
-                                start={0}
-                                end={countryInfo.todayCases}
-                                duration={2.5}
-                                separator=","
-                            />
+                        <Typography color="textSecondary" gutterBottom>Today Infected: <CountUp start={0} end={countryInfo.todayCases} duration={2.5} separator=","/>
                         </Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} xs={12} md={3}>
+                <Grid item component={Card} xs={12} md={3.15} className={styles.card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">
-                            <CountUp start={0} end={countryInfo.recovered} duration={2.5} separator=","/>
-                        </Typography>
-                        <Typography color="textSecondary" gutterBottom>Today Recovered: <CountUp start={0} end={countryInfo.todayRecovered} duration={2.5}separator=","/>
-                        </Typography>
+                        <Typography variant="h5"><CountUp start={0} end={countryInfo.recovered} duration={2.5} separator=","/></Typography>
+                        <Typography color="textSecondary" gutterBottom>Today Recovered: <CountUp start={0} end={countryInfo.todayRecovered} duration={2.5}separator=","/></Typography>
                         <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} xs={12} md={3}>
+                <Grid item component={Card} xs={12} md={3.15} className={styles.card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp start={0} end={countryInfo.deaths} duration={2.5} separator=","/>
