1f4ed678580b6a1b05b5e3a2793a2057a53018fe
Lewis Potticary <lewispotticary@gmail.com>
2021-10-19 23:37:16 +0100

    Added map component to index

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Chart } from './Chart/Chart';
 export { default as CountryPicker } from './CountryPicker/CountryPicker';
 export { default as CountrySelected} from './CountrySelected/CountrySelected';
 export { default as Table} from './Table/Table';
+export { default as Map} from './Map/Map';
